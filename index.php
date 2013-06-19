fb6caf280789d84af65e788dce1db448c3a5ecac
Abubakar Saad <abubakar.saad.891@gmail.com>
2013-06-19 16:43:26 -0400

    index includes bootstraps

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <html>
 <head>
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-responsive.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
 	<script type="text/javascript" src="jquery-2.0.2.min.js"></script>
 	<script type="text/javascript" src="js/validator.js"></script>
 </head>
