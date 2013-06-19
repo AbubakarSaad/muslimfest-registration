0528d5656094b21f60b06aaa154b3c9630d25258
Rashaad Ramdeen <rashaad.ramdeen@gmail.com>
2013-06-19 16:37:01 -0400

    Adding and linking assets

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<script type="text/javascript" src="jquery-2.0.2.min.js"></script>
+	<script type="text/javascript" src="js/validator.js"></script>
+</head>
+<body>
+
+</body>
+</html>
