5f14fa05bc42ebf8496b3fe15215d4f65b123a2e
Abubakar Saad <abubakar.saad.891@gmail.com>
2013-06-19 17:12:05 -0400

    added html elements

--- index.php
+++ index.php
@@ -2,12 +2,36 @@
 <head>
 	<title></title>
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-responsive.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
 	<script type="text/javascript" src="jquery-2.0.2.min.js"></script>
 	<script type="text/javascript" src="js/validator.js"></script>
 </head>
 <body>
-
+	<div class="container">
+		<div class="">
+			<div id="logo">
+				logo
+			</div>
+			<div id="form-header">
+				<h2>Volunteer Registration</h2>
+				<hr>
+				<p>First Name* <br/> 
+					<input type="text" name="first-name" id="first-name"/>
+				</p>
+				<p>Last Name* <br/> 
+					<input type="text" name="last-name"  id="last-name"/>
+				</p>
+				<p>Gender
+					<select>
+						<option>select</option>
+						<option>Male</option>
+						<option>Female</option>
+					</select>
+				</p>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
