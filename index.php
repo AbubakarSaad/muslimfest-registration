0aabf4fe563b2fc2b30ec91298d94550b07ebe9d
Rashaad Ramdeen <rashaad.ramdeen@gmail.com>
2013-06-19 17:39:09 -0400

    Styling layout

--- index.php
+++ index.php
@@ -1,36 +1,37 @@
 <html>
 <head>
 	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-responsive.css">
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<script type="text/javascript" src="jquery-2.0.2.min.js"></script>
 	<script type="text/javascript" src="js/validator.js"></script>
 </head>
 <body>
-	<div class="container">
-		<div class="">
+	<div id="page" class="container-narrow">
+		<div class="row-fluid">
 			<div id="logo">
-				logo
+				<img src="img/muslimfest_logo_mississauga2.png" width="232" height="69">
 			</div>
-			<div id="form-header">
-				<h2>Volunteer Registration</h2>
-				<hr>
-				<p>First Name* <br/> 
-					<input type="text" name="first-name" id="first-name"/>
-				</p>
-				<p>Last Name* <br/> 
-					<input type="text" name="last-name"  id="last-name"/>
-				</p>
-				<p>Gender
-					<select>
-						<option>select</option>
-						<option>Male</option>
-						<option>Female</option>
-					</select>
-				</p>
-			</div>
+		</div>
+		<div id="form" class="row-fluid">
+			<h3>Volunteer Registration</h3>
+			<hr>
+			<p>First Name* <br/> 
+				<input type="text" name="first-name" id="first-name"/>
+			</p>
+			<p>Last Name* <br/> 
+				<input type="text" name="last-name"  id="last-name"/>
+			</p>
+			<p>Gender
+				<select>
+					<option>select</option>
+					<option>Male</option>
+					<option>Female</option>
+				</select>
+			</p>
 		</div>
 	</div>
 </body>
